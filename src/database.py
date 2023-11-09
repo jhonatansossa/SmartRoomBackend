a327f52c193229e5b459985d8266382f6fbe1e9b
dfabbroni <118130226+dfabbroni@users.noreply.github.com>
2023-11-09 13:42:48 -0500

    fix and add endpoints, database models

--- src/database.py
+++ src/database.py
@@ -18,98 +18,23 @@ class User(db.Model):
     def __repr__(self) -> str:
         return 'User >>> {self.username}'
 
+class DeviceItemMeasurement(db.Model):
+    thing_id = db.Column(db.Integer, primary_key=True)
+    item_id = db.Column(db.Integer, primary_key=True)
+    thing_name = db.Column(db.String(500), nullable=False)
+    item_name = db.Column(db.String(500), nullable=False)
+    measurement_name = db.Column(db.String(500), nullable=False)
 
-class Bookmark(db.Model):
-    id=db.Column(db.Integer, primary_key=True)
-    body=db.Column(db.Text, nullable=True)
-    url=db.Column(db.Text, nullable=False)
-    short_url=db.Column(db.String(3), nullable=True)
-    visits=db.Column(db.Integer, default=0)
-    user_id=db.Column(db.Integer, db.ForeignKey('user.id'))
-    created_at=db.Column(db.DateTime, default=datetime.now())
-    updated_at=db.Column(db.DateTime, onupdate=datetime.now())
-
-    def generate_short_characters(self):
-        characters=string.digits+string.ascii_letters
-        picked_chars=''.join(random.choices(characters, k=3))
-
-        link=self.query.filter_by(short_url=picked_chars).first()
-
-        if link:
-            self.generate_short_characters()
-        else:
-            return picked_chars
-
-
-    def __init__(self, **kwargs) -> None:
-        super().__init__(**kwargs)
-
-        self.short_url=self.generate_short_characters()
-
-    def __repr__(self) -> str:
-        return 'Bookmark >>> {self.url}'
-
-
-class Relations(db.Model):
-    ID=db.Column(db.String(2), primary_key=True)
-    ACTIVEIMPORTID=db.Column(db.String(2), nullable=True)
-    ACTIVEEXPORTID=db.Column(db.String(2), nullable=True)
-    REACTIVEIMPORTID=db.Column(db.String(2), nullable=True)
-    REACTIVEEXPORTID=db.Column(db.String(2), nullable=True)
-    APPARENTIMPORTID=db.Column(db.String(2), nullable=True)
-    APPARENTEXPORTID=db.Column(db.String(2), nullable=True)
+    def __repr__(self):
+        return 'Thing Id: {self.thing_id}, Item Id: {self.item_id}, Thing: {self.thing.name}, Item: {self.item.name}, Measurement: {self.measurement.name}'
     
-
-    def __repr__(self) -> str:
-        return 'Id >>> {self.ID}'
-
     @property
     def serialize(self):
        """Return object data in easily serializable format"""
        return {
-           'id'         : self.ID,
-           'ACTIVEIMPORTID'         : self.ACTIVEIMPORTID,
-           'ACTIVEEXPORTID'         : self.ACTIVEEXPORTID,
-           'REACTIVEIMPORTID'         : self.REACTIVEIMPORTID,
-           'REACTIVEEXPORTID'         : self.REACTIVEEXPORTID,
-           'APPARENTIMPORTID'         : self.APPARENTIMPORTID,
-           'APPARENTEXPORTID'         : self.APPARENTEXPORTID
+            'thing_id': self.thing_id,
+            'item_id': self.item_id,
+            'device_name': self.device_name,
+            'item_name': self.item_name,
+            'measurement_name': self.measurement_name
        }
-
-
-class Names(db.Model):
-    ID=db.Column(db.String(2), primary_key=True)
-    NAME=db.Column(db.String(30), nullable=True)
-
-    def __repr__(self) -> str:
-        return 'Name >>> {self.NAME}'
-
-    @property
-    def serialize(self):
-       """Return object data in easily serializable format"""
-       return {
-           'id'         : self.ID,
-           'name': self.NAME
-       }
-
-
-def create_item_models(table_name):
-    class Item(db.Model):
-        __tablename__  = table_name 
-        __table_args__ = {'extend_existing': True}
-        TIME= db.Column(db.DateTime, primary_key=True, nullable=False) 
-        VALUE= db.Column(db.Float, nullable=True)
-
-        @property 
-        def serialize(self):
-            "Return object data in easily serializable format"""
-            return {
-                'time' : self.TIME, 'value': self.VALUE
-            }
-    return Item
-
-
-
-
-
-
--- src/devices.py
+++ src/devices.py
@@ -1,33 +1,105 @@
 from flask import Blueprint, jsonify, request, make_response
 from src.constants.http_status_codes import HTTP_200_OK, HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND, HTTP_202_ACCEPTED, HTTP_503_SERVICE_UNAVAILABLE
-from src.database import Relations, Names, db, create_item_models
-from src.database import User
+from src.database import db, DeviceItemMeasurement
 from sqlalchemy import func
 from flasgger import swag_from
 import requests 
 import os
 from flask_jwt_extended import jwt_required
+from urllib.parse import quote
 
 
 devices = Blueprint("devices", __name__, url_prefix="/api/v1/devices")
-
-energy_types = {'active_import_energy': 'ACTIVEIMPORTID', 'active_export_energy': 'ACTIVEEXPORTID', 
-                'reactive_import_energy': 'REACTIVEIMPORTID', 'reactive_export_energy': 'REACTIVEEXPORTID',  
-                'apparent_import_energy': 'APPARENTIMPORTID', 'apparent_export_energy': 'APPARENTEXPORTID'}
 
 OPENHAB_URL=os.environ.get("OPENHAB_URL")
 OPENHAB_PORT=os.environ.get("OPENHAB_PORT")
 username=os.environ.get("USERNAME")
 password=os.environ.get("PASSWORD")
 
+@devices.get('/loadmetadata')
+@jwt_required()
+@swag_from('./docs/devices/get_metadata.yml')
+def get_metadata():
+    items = requests.get('https://'+OPENHAB_URL+':'+OPENHAB_PORT+'/rest/items?recursive=false', auth=(username, password))
+    if items is None:
+        response = make_response(jsonify({
+                'error': 'The service is not available'
+            }))
+        response.status_code = HTTP_503_SERVICE_UNAVAILABLE
+        return response
+    
+    items_converted = items.json()
+
+    db.session.query(DeviceItemMeasurement).delete()
+    db.session.commit()
+
+    for item in items_converted:
+        label = item['label'].split('_')
+
+        thing_id = label[0]
+        item_id = label[1]
+        thing_name = label[0] + "_" + label[2]
+        item_name = item['name']
+        measurement_name = label[3]
+
+        entry = DeviceItemMeasurement(thing_id = thing_id, item_id = item_id, thing_name = thing_name, item_name = item_name, measurement_name = measurement_name)
+        db.session.add(entry)
+        db.session.commit()
+
+    return jsonify({
+    'message': "Success"
+    }), HTTP_200_OK
+
+
+@devices.get('/relations')
+@jwt_required()
+@swag_from('./docs/devices/relations.yml')
+def get_all_relations():
+    result = DeviceItemMeasurement.query.all()
+
+    if result is None:
+        response = make_response(jsonify({
+                'error': 'The service is not available'
+            }))
+        response.status_code = HTTP_503_SERVICE_UNAVAILABLE
+        return response
+
+    output = []
+
+    for thing_id, item_id, device_name, item_name, measurement_name in result:
+        output.append({'thing_id': thing_id, 'item_id': item_id, 'device_name': device_name, 'item_name': item_name, 'measurement_name': measurement_name})
+    
+    return jsonify(output), HTTP_200_OK
+
+
+@devices.get("/relations/<thingid>")
+@jwt_required()
+@swag_from('./docs/devices/relations_id.yml')
+def get_thing_relations(thing_id):
+    result = DeviceItemMeasurement.query.filter_by(thing_id = thing_id).all()
+
+    if result is None:
+        response = make_response(jsonify({
+                'error': 'The service is not available'
+            }))
+        response.status_code = HTTP_503_SERVICE_UNAVAILABLE
+        return response
+
+    output = []
+
+    for thing_id, item_id, device_name, item_name, measurement_name in result:
+        output.append({'thing_id': thing_id, 'item_id': item_id, 'device_name': device_name, 'item_name': item_name, 'measurement_name': measurement_name})
+    
+    return jsonify(output), HTTP_200_OK
+
 
 @devices.get('/items')
 @jwt_required()
 @swag_from('./docs/devices/get_all.yml')
 def get_all():
-    items = requests.get('https://'+OPENHAB_URL+':'+OPENHAB_PORT+'/rest/items?recursive=false', auth=(username, password))
+    items = requests.get('https://'+OPENHAB_URL+':'+OPENHAB_PORT+'/rest/items?recursive=false&fields=name,state,label,editable', auth=(username, password))
     if items.ok:
-        return items.json(), HTTP_200_OK
+        return jsonify(items), HTTP_200_OK
     else:
         response = make_response(jsonify({
                 'error': 'The service is not available'
@@ -36,20 +108,31 @@ def get_all():
         return response
 
 
-@devices.get("/items/<itemname>")
+@devices.get("/items/<itemid>")
 @jwt_required()
 @swag_from('./docs/devices/item_id.yml')
-def item_id(itemname):
-    itemname=itemname
-    info = requests.get('https://'+OPENHAB_URL+':'+OPENHAB_PORT+'/rest/items/'+itemname+'?recursive=true', auth=(username, password))
-    if info.ok:
-        return info.json(), HTTP_200_OK
-    elif info.status_code == 404:
+def item_id(itemid):
+    item = DeviceItemMeasurement.query.filter_by(id_item = itemid).first()
+    if item is None:
         response = make_response(jsonify({
-                'error': info.json()['error']['message']
-            }))
-        response.status_code=info.status_code
+        'error': 'Item not found'
+        }))
+        response.status_code=HTTP_404_NOT_FOUND
         return response
+    
+    info = requests.get('https://'+OPENHAB_URL+':'+OPENHAB_PORT+'/rest/items/'+item.name+'?recursive=true', auth=(username, password))
+
+    if info.ok:
+        info = info.json()
+        info['id'] = item.name
+        return jsonify(info), HTTP_200_OK
+    
+    response = make_response(jsonify({
+            'error': info.json()['error']['message']
+        }))
+    response.status_code=info.status_code
+    return response
+
 
 @devices.post("/items/<itemname>/state")
 @jwt_required()
@@ -77,38 +160,38 @@ def change_state(itemname):
 @jwt_required()
 @swag_from('./docs/devices/last_measurement.yml')
 def last_measurement():
-    ID=request.json.get('id', '')
-    measurement=request.json.get('measurement', '')
-    start_time=request.json.get('start_time', '')
-    end_time=request.json.get('end_time', '')
+    id_thing = request.json.get('thing_id', '')
+    measurement = request.json.get('measurement', '')
+    start_time = request.json.get('start_time', '')
+    end_time = request.json.get('end_time', '')
 
     if start_time >= end_time:
         response = make_response(jsonify({
             'error': 'The start time is greater than end time'
         }))
         response.status_code=HTTP_400_BAD_REQUEST
-    else: 
-        names=Names.query.filter_by(ID=ID).first()
-        if names:
-            if measurement in energy_types:
-                relations = Relations.query.filter_by(ID=ID).first()
-                col_name = energy_types[measurement]
-                
-                id = relations.serialize[col_name]
+        return response
+    
+    entry = DeviceItemMeasurement.query.filter_by(id_thing = id_thing, measurement_name = measurement).first()
 
-                if id:
-                    item = create_item_models('item00'+str(id))
-                    #query = item.query.filter(item.TIME >= start_time, item.TIME <= end_time).all()
-                    query = item.query.with_entities(func.date_format(item.TIME, '%Y-%m-%d %H'), func.avg(item.VALUE)).filter(item.TIME >= start_time, item.TIME <= end_time).group_by(func.date_format(item.TIME, '%Y-%m-%d %H')).all()
-                    response = make_response(jsonify(lastMeasurements=[tuple(row) for row in query]))
-                    response.status_code=HTTP_200_OK
-                else:
-                    response = make_response(jsonify({'error': 'The measurement was not found'}))
-                    response.status_code=HTTP_404_NOT_FOUND
-            else:
-                response = make_response(jsonify({'error': 'That measurement does not exist'}))
-                response.status_code=HTTP_404_NOT_FOUND
-        else: 
-            response = make_response(jsonify({'error': 'Wrong id'}))
-            response.status_code = HTTP_400_BAD_REQUEST
-    return response
+    if entry is None:
+        response = make_response(jsonify({
+            'error': "Measurement not found"
+        }))
+        response.status_code=HTTP_404_NOT_FOUND
+        return response
+    
+    item_name = entry.item_name
+    start_time_formatted = quote(start_time.replace(" ", "-"))
+    end_time_formatted = quote(end_time.replace(" ", "-"))
+
+    response = requests.get('https://'+OPENHAB_URL+':'+OPENHAB_PORT+'/rest/persistence/items/'+item_name+'?starttime='+start_time_formatted+'&endtime='+end_time_formatted, auth=(username, password))
+
+    if response.ok:
+        return jsonify(response.json()), HTTP_200_OK
+
+    ans = make_response(jsonify({
+            'error': response.json()['error']['message']
+        }))
+    ans.status_code=response.status_code
+    return ans
